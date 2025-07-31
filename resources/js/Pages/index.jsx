9a7c6dbccc09e6eecca1def1b3a7c9f3788b1884
triptorahman <triptorahman@gmail.com>
2025-07-31 22:07:56 +0600

    ➖ dep: Remove unused library 'old-dependency'

--- resources/js/Pages/index.jsx
+++ resources/js/Pages/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "../assets/vendor/bootstrap/css/bootstrap.min.css";
 import "../assets/vendor/bootstrap-icons/bootstrap-icons.css";
-import "../assets/vendor/glightbox/css/glightbox.min.css";
-import "../assets/vendor/swiper/swiper-bundle.min.css";
 import "../assets/css/main.css";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
