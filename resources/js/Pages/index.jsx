9a8906cf395029d953c779ec5a30632f152fde75
triptorahman <triptorahman@gmail.com>
2025-07-31 22:20:37 +0600

    :pencil2: remove js and implement to react

--- resources/js/Pages/index.jsx
+++ resources/js/Pages/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../assets/vendor/bootstrap/css/bootstrap.min.css";
 import "../assets/vendor/bootstrap-icons/bootstrap-icons.css";
 import "../assets/css/main.css";
@@ -12,38 +12,62 @@ import ServicesSection from "./ServicesSection";
 import ContactSection from "./ContactSection";
 import Footer from "./Footer";
 
-const IndexPage = () => (
-  <>
-    <Header />
-    <main className="main">
+const IndexPage = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      document.querySelectorAll('.navmenu a').forEach(link => {
+        if (!link.hash) return;
+        const section = document.querySelector(link.hash);
+        if (!section) return;
+        const position = window.scrollY + 200;
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+          link.classList.add('active');
+        } else {
+          link.classList.remove('active');
+        }
+      });
+    };
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('load', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('load', handleScroll);
+    };
+  }, []);
 
-      {/* Hero Section */}
-      <HeroSection />
+  return (
+    <>
+      <Header />
+      <main className="main">
 
-      {/* About Section */}
-      <AboutSection />
+        {/* Hero Section */}
+        <HeroSection />
 
-      {/* Skills Section */}
-      <SkillsSection />
+        {/* About Section */}
+        <AboutSection />
 
-      {/* Resume Section */}
-      <ResumeSection />
+        {/* Skills Section */}
+        <SkillsSection />
 
-      {/* Portfolio Section */}
-      <PortfolioSection />
+        {/* Resume Section */}
+        <ResumeSection />
 
-      {/* Services Section */}
-      <ServicesSection />
+        {/* Portfolio Section */}
+        <PortfolioSection />
 
-      {/* Contact Section */}
-      <ContactSection />
+        {/* Services Section */}
+        <ServicesSection />
 
-    </main>
+        {/* Contact Section */}
+        <ContactSection />
 
-    <Footer />
+      </main>
 
-    
-  </>
-);
+      <Footer />
+
+      
+    </>
+  );
+};
 
 export default IndexPage;
